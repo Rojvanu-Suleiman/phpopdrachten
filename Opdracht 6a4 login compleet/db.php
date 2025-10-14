8b31d042c681620e927406cb263d62ba49c8fd03
rojvanu-suleiman <9025061@student.zadkine.nl>
2025-10-14 14:38:14 +0200

    Update db.php

--- Opdracht 6a4 login compleet/db.php
+++ Opdracht 6a4 login compleet/db.php
@@ -1,12 +1,38 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "login";
+// db.php
+// Verantwoordelijk voor database connectie
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+class Database {
+    private $host = "localhost";
+    private $dbname = "login";
+    private $username = "root";
+    private $password = "";
+    private static $instance = null;
+    private $conn;
 
-if ($conn->connect_error) {
-    die("Verbinding mislukt: " . $conn->connect_error);
+    private function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Verbinding mislukt: " . $e->getMessage());
+        }
+    }
+
+    // Singleton (altijd 1 verbinding)
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
 }
 ?>
